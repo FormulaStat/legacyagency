4d6f243574b6afdc2ec0b50745af1df7c7f77e18
FormulaStat <kaskyjames@gmail.com>
2025-07-30 15:44:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Toggle navbar links on mobile
-const toggleMenu = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
 
-toggleMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
